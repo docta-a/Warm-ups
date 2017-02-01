2b84a0df08bf5738249235a24b096073bdf8dbe8
Game110AM <Game110AM@605-110-STU20>
2017-02-01 08:36:42 -0700

    hi souchen jk

---
+++ Febuary warm up/Febuary warm up/YorN.cpp
@@ -0,0 +1,13 @@
+#include<iostream>//library
+using namespace std;//cheat coe
+int main () {//starting point
+
+char input;
+	
+	cout <<"do you like the 49ers?" << endl;
+if (input == 'n')
+	cout << "you suck!!!" << endl;
+if (input == 'y')
+	cout << "you dont suck!" << endl;
+
+}
