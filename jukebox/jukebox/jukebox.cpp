6261191943a36ad0ca0e3653a19c40458f2e993d
Game110AM <Game110AM@605-110-STU20>
2017-02-02 08:17:20 -0700

    hi

---
+++ jukebox/jukebox/jukebox.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char songs;
+
+	cout << "Choose your song" << endl;
+
+	cout << "a=Soundcloud by Silent Broo" << endl;
+
+	cout << "b=Redbone by Childish Gambino" << endl;
+
+	cout << "c=Alright by Kendrick Lamar" << endl;
+
+	cin >> songs;
+	if (songs == 'a')
+		cout << "Ugh chillen with my homiez" << endl;
+	else if (songs == 'b')
+		cout << "My Penutbutter Choclate cake with Kool-aid'" << endl;
+	else if (songs == 'c')
+		cout << "**** we Gon be alright!!" << endl;
+
+}
